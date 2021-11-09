9a1bdc87b6cc653dc64bbbc587f6bb2930b73a8a
ricksterhd123 <ricksterhd123@gmail.com>
2021-11-09 02:12:56 +0000

    Add global style back to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import theme from './theme';
-// import '../styles/globals.css'
+import theme from '../theme';
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
